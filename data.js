ccce47d38cf54441b2f11b9f9164a3c4d8ebf0c8
Farhan Sangaji <farhan11soyae@gmail.com>
2024-06-18 03:57:28 +0700

    initial web

---
+++ data.js
@@ -0,0 +1,862 @@
+window.data = {
+    "champions": [
+        {
+            "name": "Aatrox",
+            "traits": [
+                "Ghostly",
+                "Inkshadow",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_Aatrox"
+        },
+        {
+            "name": "Ahri",
+            "traits": [
+                "Fated",
+                "Arcanist"
+            ],
+            "apiName": "TFT11_Ahri"
+        },
+        {
+            "name": "Amumu",
+            "traits": [
+                "Porcelain",
+                "Warden"
+            ],
+            "apiName": "TFT11_Amumu"
+        },
+        {
+            "name": "Annie",
+            "traits": [
+                "Fortune",
+                "Invoker"
+            ],
+            "apiName": "TFT11_Annie"
+        },
+        {
+            "name": "Aphelios",
+            "traits": [
+                "Fated",
+                "Sniper"
+            ],
+            "apiName": "TFT11_Aphelios"
+        },
+        {
+            "name": "Ashe",
+            "traits": [
+                "Porcelain",
+                "Sniper"
+            ],
+            "apiName": "TFT11_Ashe"
+        },
+        {
+            "name": "Alune",
+            "traits": [
+                "Umbral",
+                "Invoker"
+            ],
+            "apiName": "TFT11_Alune"
+        },
+        {
+            "name": "Bard",
+            "traits": [
+                "Mythic",
+                "Trickshot"
+            ],
+            "apiName": "TFT11_Bard"
+        },
+        {
+            "name": "Caitlyn",
+            "traits": [
+                "Ghostly",
+                "Sniper"
+            ],
+            "apiName": "TFT11_Caitlyn"
+        },
+        {
+            "name": "Cho'Gath",
+            "traits": [
+                "Mythic",
+                "Behemoth"
+            ],
+            "apiName": "TFT11_ChoGath"
+        },
+        {
+            "name": "Darius",
+            "traits": [
+                "Umbral",
+                "Duelist"
+            ],
+            "apiName": "TFT11_Darius"
+        },
+        {
+            "name": "Galio",
+            "traits": [
+                "Storyweaver",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_Galio"
+        },
+        {
+            "name": "Garen",
+            "traits": [
+                "Storyweaver",
+                "Warden"
+            ],
+            "apiName": "TFT11_Garen"
+        },
+        {
+            "name": "Hwei",
+            "traits": [
+                "Mythic",
+                "Artist"
+            ],
+            "apiName": "TFT11_Hwei"
+        },
+        {
+            "name": "Illaoi",
+            "traits": [
+                "Ghostly",
+                "Arcanist",
+                "Warden"
+            ],
+            "apiName": "TFT11_Illaoi"
+        },
+        {
+            "name": "Irelia",
+            "traits": [
+                "Storyweaver",
+                "Duelist"
+            ],
+            "apiName": "TFT11_Irelia"
+        },
+        {
+            "name": "Janna",
+            "traits": [
+                "Dragonlord",
+                "Invoker"
+            ],
+            "apiName": "TFT11_Janna"
+        },
+        {
+            "name": "Kai'Sa",
+            "traits": [
+                "Inkshadow",
+                "Trickshot"
+            ],
+            "apiName": "TFT11_Kaisa"
+        },
+        {
+            "name": "Kayn",
+            "traits": [
+                "Ghostly",
+                "Reaper"
+            ],
+            "apiName": "TFT11_Kayn"
+        },
+        {
+            "name": "Kha'Zix",
+            "traits": [
+                "Heavenly",
+                "Reaper"
+            ],
+            "apiName": "TFT11_KhaZix"
+        },
+        {
+            "name": "Kindred",
+            "traits": [
+                "Fated",
+                "Dryad",
+                "Reaper"
+            ],
+            "apiName": "TFT11_Kindred"
+        },
+        {
+            "name": "Kog'Maw",
+            "traits": [
+                "Mythic",
+                "Invoker",
+                "Sniper"
+            ],
+            "apiName": "TFT11_KogMaw"
+        },
+        {
+            "name": "Lee Sin",
+            "traits": [
+                "Dragonlord",
+                "Duelist"
+            ],
+            "apiName": "TFT11_LeeSin"
+        },
+        {
+            "name": "Lillia",
+            "traits": [
+                "Mythic",
+                "Invoker"
+            ],
+            "apiName": "TFT11_Lillia"
+        },
+        {
+            "name": "Lissandra",
+            "traits": [
+                "Porcelain",
+                "Arcanist"
+            ],
+            "apiName": "TFT11_Lissandra"
+        },
+        {
+            "name": "Lux",
+            "traits": [
+                "Porcelain",
+                "Arcanist"
+            ],
+            "apiName": "TFT11_Lux"
+        },
+        {
+            "name": "Malphite",
+            "traits": [
+                "Heavenly",
+                "Behemoth"
+            ],
+            "apiName": "TFT11_Malphite"
+        },
+        {
+            "name": "Morgana",
+            "traits": [
+                "Ghostly",
+                "Sage"
+            ],
+            "apiName": "TFT11_Morgana"
+        },
+        {
+            "name": "Neeko",
+            "traits": [
+                "Mythic",
+                "Heavenly",
+                "Arcanist"
+            ],
+            "apiName": "TFT11_Neeko"
+        },
+        {
+            "name": "Ornn",
+            "traits": [
+                "Dryad",
+                "Behemoth"
+            ],
+            "apiName": "TFT11_Ornn"
+        },
+        {
+            "name": "Qiyana",
+            "traits": [
+                "Heavenly",
+                "Duelist"
+            ],
+            "apiName": "TFT11_Qiyana"
+        },
+        {
+            "name": "Senna",
+            "traits": [
+                "Inkshadow",
+                "Sniper"
+            ],
+            "apiName": "TFT11_Senna"
+        },
+        {
+            "name": "Sett",
+            "traits": [
+                "Fated",
+                "Umbral",
+                "Warden"
+            ],
+            "apiName": "TFT11_Sett"
+        },
+        {
+            "name": "Shen",
+            "traits": [
+                "Ghostly",
+                "Behemoth"
+            ],
+            "apiName": "TFT11_Shen"
+        },
+        {
+            "name": "Riven",
+            "traits": [
+                "Storyweaver",
+                "Altruist",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_Riven"
+        },
+        {
+            "name": "Sivir",
+            "traits": [
+                "Storyweaver",
+                "Trickshot"
+            ],
+            "apiName": "TFT11_Sivir"
+        },
+        {
+            "name": "Soraka",
+            "traits": [
+                "Heavenly",
+                "Altruist"
+            ],
+            "apiName": "TFT11_Soraka"
+        },
+        {
+            "name": "Sylas",
+            "traits": [
+                "Umbral",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_Sylas"
+        },
+        {
+            "name": "Syndra",
+            "traits": [
+                "Fated",
+                "Arcanist"
+            ],
+            "apiName": "TFT11_Syndra"
+        },
+        {
+            "name": "Tahm Kench",
+            "traits": [
+                "Mythic",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_TahmKench"
+        },
+        {
+            "name": "Teemo",
+            "traits": [
+                "Fortune",
+                "Trickshot"
+            ],
+            "apiName": "TFT11_Teemo"
+        },
+        {
+            "name": "Thresh",
+            "traits": [
+                "Fated",
+                "Behemoth"
+            ],
+            "apiName": "TFT11_Thresh"
+        },
+        {
+            "name": "Tristana",
+            "traits": [
+                "Fortune",
+                "Duelist"
+            ],
+            "apiName": "TFT11_Tristana"
+        },
+        {
+            "name": "Udyr",
+            "traits": [
+                "Inkshadow",
+                "Behemoth",
+                "Spirit Walker"
+            ],
+            "apiName": "TFT11_Udyr"
+        },
+        {
+            "name": "Volibear",
+            "traits": [
+                "Inkshadow",
+                "Duelist"
+            ],
+            "apiName": "TFT11_Volibear"
+        },
+        {
+            "name": "Wukong",
+            "traits": [
+                "Great",
+                "Heavenly",
+                "Sage"
+            ],
+            "apiName": "TFT11_WuKong"
+        },
+        {
+            "name": "Yasuo",
+            "traits": [
+                "Fated",
+                "Duelist"
+            ],
+            "apiName": "TFT11_Yasuo"
+        },
+        {
+            "name": "Yone",
+            "traits": [
+                "Umbral",
+                "Reaper"
+            ],
+            "apiName": "TFT11_Yone"
+        },
+        {
+            "name": "Yorick",
+            "traits": [
+                "Umbral",
+                "Behemoth"
+            ],
+            "apiName": "TFT11_Yorick"
+        },
+        {
+            "name": "Zoe",
+            "traits": [
+                "Fortune",
+                "Storyweaver",
+                "Arcanist"
+            ],
+            "apiName": "TFT11_Zoe"
+        },
+        {
+            "name": "Zyra",
+            "traits": [
+                "Storyweaver",
+                "Sage"
+            ],
+            "apiName": "TFT11_Zyra"
+        },
+        {
+            "name": "Kobuko",
+            "traits": [
+                "Fortune",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_FortuneYord"
+        },
+        {
+            "name": "Rek'Sai",
+            "traits": [
+                "Dryad",
+                "Bruiser"
+            ],
+            "apiName": "TFT11_RekSai"
+        },
+        {
+            "name": "Xayah",
+            "traits": [
+                "Dragonlord",
+                "Lovers",
+                "Trickshot"
+            ],
+            "apiName": "TFT11_Xayah"
+        },
+        {
+            "name": "Gnar",
+            "traits": [
+                "Dryad",
+                "Warden"
+            ],
+            "apiName": "TFT11_Gnar"
+        },
+        {
+            "name": "Rakan",
+            "traits": [
+                "Dragonlord",
+                "Lovers",
+                "Altruist"
+            ],
+            "apiName": "TFT11_Rakan"
+        },
+        {
+            "name": "Jax",
+            "traits": [
+                "Inkshadow",
+                "Warden"
+            ],
+            "apiName": "TFT11_Jax"
+        },
+        {
+            "name": "Diana",
+            "traits": [
+                "Dragonlord",
+                "Sage"
+            ],
+            "apiName": "TFT11_Diana"
+        },
+        {
+            "name": "Azir",
+            "traits": [
+                "Dryad",
+                "Invoker"
+            ],
+            "apiName": "TFT11_Azir"
+        },
+        {
+            "name": "Nautilus",
+            "traits": [
+                "Mythic",
+                "Warden"
+            ],
+            "apiName": "TFT11_Nautilus"
+        },
+    ],
+    "traits": [
+        {
+          "name": "Ghostly",
+          "minUnits": [
+            2,
+            4,
+            6,
+            8
+          ],
+          "maxUnits": [
+            3,
+            5,
+            7,
+            25000
+          ]
+        },
+        {
+          "name": "Fortune",
+          "minUnits": [
+            3,
+            5,
+            7
+          ],
+          "maxUnits": [
+            4,
+            6,
+            25000
+          ]
+        },
+        {
+          "name": "Lovers",
+          "minUnits": [
+            1
+          ],
+          "maxUnits": [
+            1
+          ]
+        },
+        {
+          "name": "Reaper",
+          "minUnits": [
+            2,
+            4
+          ],
+          "maxUnits": [
+            3,
+            25000
+          ]
+        },
+        {
+          "name": "Trickshot/Altruist",
+          "minUnits": [
+            1
+          ],
+          "maxUnits": [
+            25000
+          ]
+        },
+        {
+          "name": "Umbral",
+          "minUnits": [
+            2,
+            4,
+            6,
+            9
+          ],
+          "maxUnits": [
+            3,
+            5,
+            8,
+            25000
+          ]
+        },
+        {
+          "name": "Dryad",
+          "minUnits": [
+            2,
+            4,
+            6
+          ],
+          "maxUnits": [
+            3,
+            5,
+            25000
+          ]
+        },
+        {
+          "name": "Sage",
+          "minUnits": [
+            2,
+            3,
+            4,
+            5
+          ],
+          "maxUnits": [
+            2,
+            3,
+            4,
+            25000
+          ]
+        },
+        {
+          "name": "Arcanist",
+          "minUnits": [
+            2,
+            4,
+            6,
+            8
+          ],
+          "maxUnits": [
+            3,
+            5,
+            7,
+            25000
+          ]
+        },
+        {
+          "name": "Inkshadow",
+          "minUnits": [
+            3,
+            5,
+            7
+          ],
+          "maxUnits": [
+            4,
+            6,
+            25000
+          ]
+        },
+        {
+          "name": "Altruist",
+          "minUnits": [
+            2,
+            3,
+            4
+          ],
+          "maxUnits": [
+            2,
+            3,
+            25000
+          ]
+        },
+        {
+          "name": "Artist",
+          "minUnits": [
+            1
+          ],
+          "maxUnits": [
+            25000
+          ]
+        },
+        {
+          "name": "Exalted",
+          "minUnits": [
+            3,
+            5
+          ],
+          "maxUnits": [
+            4,
+            25000
+          ]
+        },
+        {
+          "name": "Heavenly",
+          "minUnits": [
+            2,
+            3,
+            4,
+            5,
+            6,
+            7
+          ],
+          "maxUnits": [
+            2,
+            3,
+            4,
+            5,
+            6,
+            25000
+          ]
+        },
+        {
+          "name": "Porcelain",
+          "minUnits": [
+            2,
+            4,
+            6
+          ],
+          "maxUnits": [
+            3,
+            5,
+            25000
+          ]
+        },
+        {
+          "name": "Duelist",
+          "minUnits": [
+            2,
+            4,
+            6,
+            8
+          ],
+          "maxUnits": [
+            3,
+            5,
+            7,
+            25000
+          ]
+        },
+        {
+          "name": "Dragonlord",
+          "minUnits": [
+            2,
+            3,
+            4,
+            5
+          ],
+          "maxUnits": [
+            2,
+            3,
+            4,
+            25000
+          ]
+        },
+        {
+          "name": "Bruiser",
+          "minUnits": [
+            2,
+            4,
+            6,
+            8
+          ],
+          "maxUnits": [
+            3,
+            5,
+            7,
+            25000
+          ]
+        },
+        {
+          "name": "Trickshot",
+          "minUnits": [
+            2,
+            4
+          ],
+          "maxUnits": [
+            3,
+            25000
+          ]
+        },
+        {
+          "name": "Fated",
+          "minUnits": [
+            3,
+            5,
+            7,
+            10
+          ],
+          "maxUnits": [
+            4,
+            6,
+            9,
+            25000
+          ]
+        },
+        {
+          "name": "Invoker",
+          "minUnits": [
+            2,
+            4,
+            6
+          ],
+          "maxUnits": [
+            3,
+            5,
+            25000
+          ]
+        },
+        {
+          "name": "Great",
+          "minUnits": [
+            1
+          ],
+          "maxUnits": [
+            25000
+          ]
+        },
+        {
+          "name": "Sniper",
+          "minUnits": [
+            2,
+            4,
+            6
+          ],
+          "maxUnits": [
+            3,
+            5,
+            25000
+          ]
+        },
+        {
+          "name": "Mythic",
+          "minUnits": [
+            3,
+            5,
+            7,
+            10
+          ],
+          "maxUnits": [
+            4,
+            6,
+            9,
+            25000
+          ]
+        },
+        {
+          "name": "Spirit Walker",
+          "minUnits": [
+            1
+          ],
+          "maxUnits": [
+            25000
+          ]
+        },
+        {
+          "name": "Warden",
+          "minUnits": [
+            2,
+            4,
+            6
+          ],
+          "maxUnits": [
+            3,
+            5,
+            25000
+          ]
+        },
+        {
+          "name": "Behemoth",
+          "minUnits": [
+            2,
+            4,
+            6
+          ],
+          "maxUnits": [
+            3,
+            5,
+            25000
+          ]
+        },
+        {
+          "name": "Storyweaver",
+          "minUnits": [
+            3,
+            5,
+            7,
+            10
+          ],
+          "maxUnits": [
+            4,
+            6,
+            9,
+            25000
+          ]
+        }
+      ]
+}
